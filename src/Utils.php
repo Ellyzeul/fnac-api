de791a976fb0ddee8ce63abc605c4a6f7d98cc4e
Ellyzeul <gabrielaug.240@gmail.com>
2023-06-24 22:17:11 -0300

    feat: tratamento de XML de resposta
    O XML retornado pela FNAC não escapa os caracteres especiais <, >, &, ', ". Manter < e & geram erro fatal, impedindo o parse do XML para DOM e escapar o > é uma boa prática, de acordo com as especificações da W3.

--- src/Utils.php
+++ src/Utils.php
@@ -1,15 +1,21 @@
 <?php
 namespace FNAC;
 
-use CurlHandle;
+use FNAC\Response;
 
 class Utils
 {
-  private static string $baseUrl = 'https://vendeur.fnac.com/api.php';
-
-  public static function postXML(string $endpoint, string $body): array
+  /**
+   * Realiza uma requisição POST enviando um XML
+   * 
+   * @param string $url URL completa para requisição
+   * @param string $body Corpo XML da requisição
+   * 
+   * @return Response Objeto contento o status de sucesso e o corpo da resposta
+   */
+  public static function postXML(string $url, string $body): Response
   {
-    $ch = Utils::getCurlHandleForPostXML(Utils::$baseUrl . $endpoint, $body);
+    $ch = Utils::getCurlHandleForPostXML($url, $body);
 
     $rawResponse = curl_exec($ch);
     $response = Utils::handlePostXMLResponse($rawResponse, $ch);
@@ -19,7 +25,7 @@ class Utils
     return $response;
   }
 
-  private static function getCurlHandleForPostXML(string $url, string $body)
+  private static function getCurlHandleForPostXML(string $url, string $body): \CurlHandle
   {
     $ch = curl_init();
 
@@ -37,24 +43,35 @@ class Utils
     return $ch;
   }
 
-  private static function handlePostXMLResponse(string | bool $response, CurlHandle $ch)
+  private static function handlePostXMLResponse(string | bool $response, \CurlHandle $ch): Response
   {
     if($response === false) {
       $errno = curl_errno($ch);
       $error = curl_error($ch);
 
-      return [
-        'success' => false, 
-        'response' => [
-          'errno' => $errno, 
-          'error' => $error
-        ]
-      ];
+      return new Response(false, [
+        'errno' => $errno, 
+        'error' => $error
+      ]);
     }
 
-    return [
-      'success' => true, 
-      'response' => simplexml_load_string($response)
-    ];
+    $escaped = Utils::escapeXMLSpecialChars($response);
+    $xml = simplexml_load_string($escaped, \SimpleXMLElement::class, LIBXML_NOCDATA);
+    $json = json_encode($xml);
+    $attrHandledJson = str_replace('@', '', $json);
+    $obj = json_decode($attrHandledJson);
+
+    return new Response(true, $obj);
+  }
+
+  private static function escapeXMLSpecialChars(string $xml): string
+  {
+    $xmlElementNamePattern = "[A-z_:][A-z0-9\-_\.:]*";
+    $xmlAttributePattern = "$xmlElementNamePattern\s*=\s*(\".*\"|'.*')";
+
+    $escaping = preg_replace('/&/', '&amp;', $xml);
+    $escaping = preg_replace("/<(?![\/]{0,1}(\?\s*){0,1}$xmlElementNamePattern\s*(\s*$xmlAttributePattern)*(\s*\?){0,1}>)/", '&lt;', $escaping);
+
+    return $escaping;
   }
 }
